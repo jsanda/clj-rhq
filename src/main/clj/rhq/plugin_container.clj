75d8337caa9ce7ee6cb3ba4e0acefcab3cd17949
John Sanda <jsanda@redhat.com>
2011-05-21 21:16:52 -0400

    inventory-filter now an arbitrary fn with the :fn option
    In addition to the options that you can specify in the filter map that is
    passed to the inventory fn, there is now a :fn option that expects a
    function that receives a Resource object as an argument and returns true if the
    resource should be included in the results.
    
    Refactored the resource-category fn making it public and making it return a
    keyword instead of an AvailabilityType object.
    
    Lastly, adding more fn docstrings.

--- src/main/clj/rhq/plugin_container.clj
+++ src/main/clj/rhq/plugin_container.clj
@@ -51,26 +51,57 @@
 
 (defn- plugin [r] (.. r getResourceType getPlugin))
 
-(defn- resource-category [r] (.. r getResourceType getCategory getName))
+(defn resource-category
+  "Returns the resource category as a keyword. Possible values are,
 
-(defn availability [r]
+    :PLATFORM
+    :SERVER
+    :SERVICE"
+  [r]
+  (keyword (.. r getResourceType getCategory getName)))
+
+(defn availability
+  "Returns the availability of a resource. Possible values are,
+
+    1) :UP
+    2) :DOWN
+  "
+  [r]
   (keyword (.name
 	    (.getAvailabilityType
 	     (.getCurrentAvailability (inventory-mgr) r)))))
 
-(defn available? [r]
+(defn available?
+  "Returns true if a resource is available, false otherwise."
+  [r]
   (= :UP (availability r)))
 
-(defn discover [& category]
-  (cond (= :server category) (.executeServerScanImmediately (inventory-mgr))
-	(= :service category) (.executeServiceScanImmediately (inventory-mgr))
-	:else [(.executeServerScanImmediately (inventory-mgr))
-	       (.executeServiceScanImmediately (inventory-mgr))]))
+(defn discover
+  "When invoked with no argument, this fn performs a both server and service
+  scans. The inventory reports are returned as a vector where the first
+  element of the vector is the report from the server scan, and the second
+  element is the report from the service scan.
+
+  When invoked with a single argument, one of two values is epxected,
+
+    1) :server
+    2) :service
+
+  where :server causes a server scan to be run and :service causes a service
+  scan to be run. Returns the inventory report. If the argument is some other
+  value, then it results this fn being invoked without any arguments."
+  ([] [(.executeServerScanImmediately (inventory-mgr))
+       (.executeServiceScanImmediately (inventory-mgr))])
+  ([category]
+     (cond (= :server category) (.executeServerScanImmediately (inventory-mgr))
+	   (= :service category) (.executeServiceScanImmediately
+				  (inventory-mgr))
+	   :else (discover))))
 
 (defn inventory-filter [opts]
   (let [filters {:plugin (fn [r] (= (plugin r) (opts :plugin)))
 		 :category (fn [r] (= (resource-category r)
-				      (name (opts :category))))
+				      (opts :category)))
 		 :availability (fn [r] (= (availability r)
 					  (opts :availability)))
 		 :type (fn [r]
@@ -79,12 +110,34 @@
 			       ; else the value of :type is assumed to be
 			       ; a string
 			       :else (= (opts :type)
-					(.. r getResourceType getName))))}]
+					(.. r getResourceType getName))))
+		 :fn (fn [r] (true? ((opts :fn) r)))}]
     (fn include-resource? [r]
       (every? (fn [f] (f r))
 	      (for [[k v] opts :when (filters k)] (filters k))))))
 
 (defn inventory
+  "Returns the platform resource object when invoked with no arguments.
+
+  Returns a lazy sequence of resources when invoked with a map of one or more
+  filter options. The supported keys for the filter options map are,
+
+    :plugin      
+    :category 
+    :avilability
+    :type
+
+  :plugin
+  Expects the plugin name as a string
+
+  :category
+  Epxects one of three values - :PLATFORM, :SERVER, or :SERVICE
+
+  :availability
+  Expects either :UP or :DOWN
+
+  :type
+  Expected either a ResourceType object, or the resource type name as a string"
   ([] (.getPlatform (inventory-mgr)))
   ([opts]
      (filter (inventory-filter opts)
